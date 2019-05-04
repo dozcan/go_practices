d32359890a4bea8c0988d21b1b0a9e2830c05aea
doga ozcan <dogasava@gmail.com>
2019-05-04 12:28:15 +0300

    Create practices.go

---
+++ practices.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+)
+
+type stack []interface{}
+
+func(s *stack) push(y interface{}) {
+     *s = append(*s,y)
+}
+
+func(s *stack) pop() (result interface {},err error) {
+   temp := *s
+   if len(temp) == 0 {
+     result = nil
+     err = errors.New("hata")
+     return
+   }
+     result = temp[0]
+     err = nil
+     temp = temp[1:]
+     *s = temp
+     return
+}
+
+func main() {
+        s := stack{}
+        s.push(4)
+        s.push("kisi") 
+        s.pop()
+	fmt.Println(s)
+}
