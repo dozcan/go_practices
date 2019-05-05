67e587a22eaa6d5d425445b859b9cec22588992e
doga ozcan <dogasava@gmail.com>
2019-05-05 16:27:32 +0300

    Update practices3.go

--- practices3.go
+++ practices3.go
@@ -32,3 +32,58 @@ func get(a int) (int) {
     } 
     panic(fmt.Sprintf("3 ile bölünemeyen sayımız %v'dır",a))
 }
+
+***********************variadic functions:****************************
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+        a:= []int{1,2,3,4}
+   	resultOfMod(4,a...)
+}
+
+func resultOfMod(a int,b ...int){
+   fmt.Println(a)
+   for _,element := range b {
+    fmt.Println(element)
+   }
+
+}
+
+*******************generic minumum fonksiyonu******************************
+i := Minimum(4, 3, 8, 2, 9).(int)
+fmt.Printf("%T %v\n", i, i)
+f := Minimum(9.4, -5.4, 3.8, 17.0, -3.1, 0.0).(float64)
+fmt.Printf("%T %v\n", f, f)
+s := Minimum("K", "X", "B", "C", "CC", "CA", "D", "M").(string)
+fmt.Printf("%T %q\n", s, s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
