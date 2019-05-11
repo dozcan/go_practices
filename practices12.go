7063d97f97834b1cc142e55bb6b269cbbddbb015
doga ozcan <dogasava@gmail.com>
2019-05-11 14:29:04 +0300

    Update practices12.go

--- practices12.go
+++ practices12.go
@@ -54,15 +54,33 @@ var sha256Hash = function (str,hashKey){
 }
 
 
-var s = "aslında ben sensiz olmam"
-var pattern = "sensiz olmam"
+
+var s = "Barry Manilow may claim to write the songs, but it was William Shakespeare who coined " 
+var s1 ="the phrases - he contributed more phrases and sayings to the English language than any "
+var s2 ="other individual, and most of them are still in daily use. Here's a collection of "
+var s3= "well-known quotations that are associated with Shakespeare. Most of these were the "
+var s4= "Bard's own work but he wasn't averse to stealing a good line occasionally and a few of "
+var s5= "these were 'popularised by' rather than 'coined by' Shakespeare"
+s = s.concat(s1,s2,s3,s4,s5)
+var pattern = "claim to"
 var l = pattern.length
 let chunk=""
 let temp= ""
 let j = 1
 let we_find_it = false
+parseMultiSpace = str => {
+  let s = str.trim().split(' ')
+  return s.reduce((prev,next) => {
+      if(next === "")
+         return prev
+      else return prev.concat(" ",next)
+  })
+}
+
 for(let i=0;i<s.length;i=i+l){
    chunk = s.substr(i,l)
+   //chunk = parseMultiSpace(chunk)
+   //pattern = parseMultiSpace(pattern)
    if(chunk !== pattern){
      temp = temp.concat(chunk)
      if(temp.length != pattern.length){
@@ -89,8 +107,10 @@ for(let i=0;i<s.length;i=i+l){
      
      
      
-     
   
+
+
+
 
 
 
