9ed4107ace6f2984f7bff85782dab1d25ed1f5c2
doga ozcan <dogasava@gmail.com>
2019-05-11 12:16:40 +0300

    Update practices12.go

--- practices12.go
+++ practices12.go
@@ -54,25 +54,46 @@ var sha256Hash = function (str,hashKey){
 }
 
 
-
-
-var s = "ben doga ama ozcan doga ozcan"
-var p = "doga ozcan"
-var l = p.length
+var s = "aslında ben sensiz olmam"
+var pattern = "sensiz olmam"
+var l = pattern.length
 let chunk=""
 let temp= ""
+let j = 1
+let we_find_it = false
 for(let i=0;i<s.length;i=i+l){
-    chunk = s.substr(i,l)
-    if(temp != "" ) {
-      console.log("pre",temp)
-      temp = temp.concat(chunk.substr(0,l-temp.length))
-      console.log("aft",temp)
-    }
-    if(chunk != p){
-       temp = 	chunk.split(' ').slice(1).join(' ')
-
-    }
-    else{
-     console.log("bulduk")
-    }
+   chunk = s.substr(i,l)
+   if(chunk !== pattern){
+     temp = temp.concat(chunk)
+     if(temp.length != pattern.length){
+       let tempPattern = temp.split(' ')
+       for(;j<tempPattern.length;j++){
+         let newIteratifTemp = tempPattern.slice(j).join(' ')
+         if(newIteratifTemp.length < pattern.length)break
+         if(newIteratifTemp.includes(pattern)){
+            console.log("we find it")  
+            we_find_it =true
+            break
+         } 
+       }
+       if(we_find_it) break
+     }
+   }
+   else {
+      console.log("we find it") 
+      break
+   }
 }
+   
+
+     
+     
+     
+     
+  
+
+
+
+
+
+
